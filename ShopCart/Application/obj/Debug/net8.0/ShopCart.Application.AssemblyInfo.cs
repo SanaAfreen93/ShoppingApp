0c3dbdf9eda34e7747649ff87e7845e84e0aefd2
Sana Afreen <sana.afreen@prutech.com>
2024-08-25 19:14:51 +0530

    commit

--- ShopCart/Application/obj/Debug/net8.0/ShopCart.Application.AssemblyInfo.cs
+++ ShopCart/Application/obj/Debug/net8.0/ShopCart.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopCart.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a945df046e778a2478a10837d733c9e39e6358c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopCart.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopCart.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
