29d78a364e4529536592fced2a4db58cb8582664
Sai Krishna Tivvari <saikrishna.tivvari@prutech.com>
2024-08-25 05:47:11 -0400

    first push

---
+++ ShopCart/Application/Dto/ResponseBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCart.Application.Dto
+{
+    public class ResponseBase<T>
+    {
+        public bool IsRequestSucessful { get; set; }
+        public string? Error { get; set; }
+
+        public T? Value { get; set; }
+    }
+}
